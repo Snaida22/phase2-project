26e5a66888fc44b52dced867130ded663a66ca5d
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-07-29 17:12:06 +0300

    fetch data

--- src/Pages/NotePage.js
+++ src/Pages/NotePage.js
@@ -3,6 +3,24 @@ import { ReactComponent as ArrowLeft } from '../Assets/arrow-left.svg'
 import { Link } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 
+function NotePage({history}) {
+
+    let [notes, setNotes] = useState([null])
+    let params = useParams()
+    // let note = notes.find(note => note.id ==`${params.id}`)
+    
+    useEffect(() =>{
+      getNote()
+    }, [params.id])
+  
+    let getNote = async() =>{
+      if(params.id === 'new') return
+      let response = await fetch(`https://json-server1131.herokuapp.com/notes/${params.id}`)
+      let data = await response.json()
+  
+      setNotes(data)
+    }
+
 return (
     <div className='note'>
     <div className='note-header'>
